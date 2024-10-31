cbc109ba8a8ae86630a9aab862938253a99fed94
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-10-31 11:28:03 +0530

    new file

---
+++ project/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def fun1(request):
+    return HttpResponse("hi")
+def fun2(request):
+    return HttpResponse("welcome")
+
+def fun3(req):
+    return render(req,'demo.html')
+
+def fun4(req):
+    return render(req,'home.html')
+
+def fun5(req):
+    return render(req,'about.html')
+
+def fun6(req):
+    return render(req,'contact.html')
+
